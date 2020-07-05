6c92357a84797d2345c9ae14dbede0281ec165a2
zjw <zjw@mails.com>
2020-07-05 02:06:26 -0700

    列表显示数据、搜索框

--- testModel/admin.py
+++ testModel/admin.py
@@ -5,12 +5,13 @@ class TagInline(admin.TabularInline):
         model = Tag
 
 class ContactAdmin(admin.ModelAdmin):
-    inlines = [TagInline]  # Inline
+    inlines = [TagInline]  # 内联，就是Tag直接在Contact页面里可以看到
+    list_display = ('name','age', 'email') # 列表显示每个数据
+    search_fields = ('name',) # 搜索框
 
     # fields 属性定义了要显示的字段。zjw check, 这样是只显示这边有的，但是在添加数据时
     # 也是一样只显示这边列的，要是没显示的项没有默认值将导致Add出错
     # fields = ('name', 'email')
-
     fieldsets = (
         ['Main',{
             'fields':('name','email'),
