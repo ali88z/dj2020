f4adbe317562a3ab6429c859a1d4ecf80c347483
zjw <zjw@mails.com>
2020-07-05 01:47:55 -0700

    显示数据库数据的部分Field

--- testModel/admin.py
+++ testModel/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from testModel.models import Test,Contact,Tag
 
-# Register your models here.
-admin.site.register([Test, Contact, Tag])
+class ContactAdmin(admin.ModelAdmin):
+    # fields 属性定义了要显示的字段。zjw check, 这样是只显示这边有的，但是在添加数据时
+    # 也是一样只显示这边列的，要是没显示的项没有默认值将导致Add出错
+    fields = ('name', 'email')
+ 
+admin.site.register(Contact, ContactAdmin)
+admin.site.register([Test, Tag])
+
