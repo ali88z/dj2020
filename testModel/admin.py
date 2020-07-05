1f0d9d1ab453d2b307cf815f860e8eaf7d2a43bd
zjw <zjw@mails.com>
2020-07-05 02:01:28 -0700

    内联(Inline)显示

--- testModel/admin.py
+++ testModel/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from testModel.models import Test,Contact,Tag
 
+class TagInline(admin.TabularInline):
+        model = Tag
+
 class ContactAdmin(admin.ModelAdmin):
+    inlines = [TagInline]  # Inline
+
     # fields 属性定义了要显示的字段。zjw check, 这样是只显示这边有的，但是在添加数据时
     # 也是一样只显示这边列的，要是没显示的项没有默认值将导致Add出错
     # fields = ('name', 'email')
@@ -18,5 +23,7 @@ class ContactAdmin(admin.ModelAdmin):
     )
  
 admin.site.register(Contact, ContactAdmin)
-admin.site.register([Test, Tag])
+# 这边没再注册Tag，因为已经内联了，没必要两个地方都显示，但要显示也是可以的，如下行
+admin.site.register([Test])
+#admin.site.register([Test, Tag])
 
